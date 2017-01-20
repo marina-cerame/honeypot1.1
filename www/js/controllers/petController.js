d53e6ea4941369afbb21da69a4a1d8ceac4f575b
Chris <christopherdakin1@gmail.com>
2017-01-19 19:32:38 -0600

    embiggened bear, added goal bar

--- www/js/controllers/petController.js
+++ www/js/controllers/petController.js
@@ -16,7 +16,12 @@ angular.module('app.pet', [])
     earUp();
   }
 
-  $scope.bar = new ProgressBar.Line(healthBar, {
+  $scope.bearGrow = function() {
+    console.log('bearGrow')
+    TweenLite.to('.bear', .1, { scale: 1.5, x: '-25%' })
+  }
+
+  $scope.healthBar = new ProgressBar.Line(healthBar, {
     strokeWidth: 4,
     easing: 'easeInOut',
     duration: 1400,
@@ -30,5 +35,47 @@ angular.module('app.pet', [])
       bar.path.setAttribute('stroke', state.color);
     }
   });
-  $scope.bar.animate(.5);
+
+  $scope.goal = new ProgressBar.Circle(goal, {
+    color: '#aaa',
+    // This has to be the same size as the maximum width to
+    // prevent clipping
+    strokeWidth: 4,
+    trailWidth: 1,
+    easing: 'easeInOut',
+    duration: 2000,
+    text: {
+      autoStyleContainer: false
+    },
+    from: { color: '#aaa', width: 1 },
+    to: { color: '#333', width: 4 },
+    // Set default step function for all animate calls
+    step: function(state, circle) {
+      circle.path.setAttribute('stroke', state.color);
+      circle.path.setAttribute('stroke-width', state.width);
+
+      var value = Math.round(circle.value() * 100);
+      if (value === 0) {
+        circle.setText('0%');
+      } else {
+        circle.setText(value + '%');
+      }
+    }
+  });
+
+  // $http({
+  // method: 'GET',
+  // url: 'http://localhost:3000/v1/levels?pet_id__is={id}'
+  // }).then(function successCallback(response) {
+  //   // this callback will be called asynchronously
+  //   // when the response is available
+  // }, function errorCallback(response) {
+  //   // called asynchronously if an error occurs
+  //   // or server returns response with an error status.
+  // });
+
+  $scope.healthBar.animate(.5);
+  $scope.goal.animate(.9)
+  $scope.bearGrow();
+
 })
