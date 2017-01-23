6019a9dd841eb10cec3a245c992221807c2b6521
gracehalbert <gracehalbert@gmail.com>
2017-01-23 17:28:58 -0600

    added my pets page

---
+++ www/js/controllers/myPetsController.js
@@ -0,0 +1,21 @@
+angular.module('myPets', [])
+.controller('MyPetsCtrl', function($scope, $rootScope, $http, $location) {
+
+  $http.get(`http://localhost:3000/v1/pets/?user_id__is=${$rootScope.user}`)
+    .then(function(res) {
+      $scope.pets = res.data.data;
+    }, function(err) {
+      console.log(err);
+    })
+
+  $scope.goToPet = function(pet) {
+    $rootScope.pet = pet;
+    $location.path('/app/pet');
+  }
+
+  $scope.editPet = function(pet) {
+    $rootScope.pet = pet;
+    $location.path('/app/editPet');
+  }
+
+})
