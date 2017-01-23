857330340e8831423b5283214104c9d7093d9a6e
gracehalbert <gracehalbert@gmail.com>
2017-01-23 14:46:12 -0600

    merging lastest verison

---
+++ platforms/ios/www/js/controllers/bankAuthController.js
@@ -0,0 +1,58 @@
+angular.module('app.bankAuth', [])
+
+    .controller('BankCtrl',
+
+        function($scope, $location, $http) {
+            $scope.checkingName;
+            $scope.savingsName;
+            var checkingHandler = Plaid.create({
+                selectAccount: true,
+                env: 'tartan',
+                clientName: 'Client Name',
+                key: 'b7491cfbd7c72652af1c7bf9c9b667',
+                product: 'auth',
+                onLoad: function() {
+                    // The Link module finished loading.
+                },
+                onSuccess: function(token, metadata) {
+                    $scope.checkingName = metadata.account.name;
+                    $scope.$apply();
+                    console.log('checking token: ', token)
+                    console.log('checking id: ', metadata.account.id);
+                },
+                onExit: function() {
+                    console.log('user closed');
+                }
+            });
+            var savingsHandler = Plaid.create({
+                selectAccount: true,
+                env: 'tartan',
+                clientName: 'Client Name',
+                key: 'b7491cfbd7c72652af1c7bf9c9b667',
+                product: 'auth',
+                onLoad: function() {
+                    // The Link module finished loading.
+                },
+                onSuccess: function(token, metadata) {
+                    $scope.savingsName = metadata.account.name;
+                    $scope.$apply();
+                    console.log('savings token: ', token);
+                    console.log('savings id: ', metadata.account.id);
+                },
+                onExit: function() {
+                    console.log('user closed');
+                }
+            });
+
+            $scope.openSavings = function() {
+                savingsHandler.open();
+            };
+            $scope.openChecking = function() {
+                checkingHandler.open();
+            };
+
+            $scope.goToFirstPet = function() {
+               $location.path('/firstPet');
+             };
+        }
+    );
