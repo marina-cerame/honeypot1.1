5e032707e037586e41d67cc088383ab1b0607a03
Marina Cerame <marina.cerame@gmail.com>
2017-01-24 15:44:19 -0600

    testing alert

--- www/js/controllers/petController.js
+++ www/js/controllers/petController.js
@@ -34,6 +34,7 @@ angular.module('app.pet', [])
   // }, 1000);
 
   $scope.bearTouch = function() {
+    alert('hello!')
     const earUp = function() {
       TweenLite.to('.ears', .5, { y: -7, onComplete: earDown })
       TweenLite.to('.leftArm', .5, {rotation:75, transformOrigin:"80% 50%"});
