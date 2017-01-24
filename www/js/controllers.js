a9ff03cfe4a4e9264d208262759142c3ebdb7974
gracehalbert <gracehalbert@gmail.com>
2017-01-23 19:36:44 -0600

    changed menu for pet page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $location) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -39,6 +39,9 @@ angular.module('starter.controllers', [])
   //     $scope.closeLogin();
   //   }, 1000);
   // };
+  $scope.goToStore = function() {
+    $location.path('/app/store');
+  }
 })
 
 .controller('PlaylistsCtrl', function($scope) {
