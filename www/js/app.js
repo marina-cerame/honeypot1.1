95254861d5880b3332926fbabb37e7d6488bc5ef
gracehalbert <gracehalbert@gmail.com>
2017-01-23 18:55:54 -0600

    some work on market menu

--- www/js/app.js
+++ www/js/app.js
@@ -43,7 +43,12 @@ angular.module('starter',['ionic',
     controller: 'AppCtrl'
   })
 
-  .state('app.pet', {
+  .state('market', {
+    url: '/market',
+    templateUrl: 'templates/marketmenu.html'
+  })
+  
+  .state('market.pet', {
     url: '/pet',
     views: {
       'menuContent': {
