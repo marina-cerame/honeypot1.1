df7df70e6dacd003e8486316402bf481c98e1a10
gracehalbert <gracehalbert@gmail.com>
2017-01-27 17:06:47 -0600

    last file for lintering

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,4 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-
-angular.module('starter',['ionic',
+angular.module('starter', ['ionic',
                           'ui.router',
                           'starter.controllers',
                           'auth',
@@ -19,8 +12,8 @@ angular.module('starter',['ionic',
                           'editPet'])
 
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
+.run(function ($ionicPlatform) {
+  $ionicPlatform.ready(function () {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
@@ -35,20 +28,20 @@ angular.module('starter',['ionic',
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function ($stateProvider, $urlRouterProvider) {
   $stateProvider
 
   .state('app', {
     url: '/app',
     abstract: true,
     templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
+    controller: 'AppCtrl',
   })
 
   .state('market', {
     url: '/market',
     templateUrl: 'templates/marketmenu.html',
-    controller: 'AppCtrl'
+    controller: 'AppCtrl',
   })
 
   .state('market.pet', {
@@ -56,15 +49,15 @@ angular.module('starter',['ionic',
     views: {
       'menuContent': {
         templateUrl: 'templates/pet.html',
-        controller: 'PetCtrl'
-      }
-    }
+        controller: 'PetCtrl',
+      },
+    },
   })
 
   .state('firstPet', {
     url: '/firstPet',
     templateUrl: 'templates/firstPet.html',
-    controller: 'FirstPetCtrl'
+    controller: 'FirstPetCtrl',
   })
 
   .state('app.adoptPet', {
@@ -72,9 +65,9 @@ angular.module('starter',['ionic',
     views: {
       'menuContent': {
         templateUrl: 'templates/adoptPet.html',
-        controller: 'adoptPetCtrl'
-      }
-    }
+        controller: 'adoptPetCtrl',
+      },
+    },
   })
 
   .state('app.store', {
@@ -82,9 +75,9 @@ angular.module('starter',['ionic',
     views: {
       'menuContent': {
         templateUrl: 'templates/store.html',
-        controller: 'StoreCtrl'
-      }
-    }
+        controller: 'StoreCtrl',
+      },
+    },
   })
 
     .state('app.account', {
@@ -92,9 +85,9 @@ angular.module('starter',['ionic',
       views: {
         'menuContent': {
           templateUrl: 'templates/account.html',
-          controller: 'accountController'
-        }
-      }
+          controller: 'accountController',
+        },
+      },
     })
 
   .state('app.editPet', {
@@ -102,9 +95,9 @@ angular.module('starter',['ionic',
     views: {
       'menuContent': {
         templateUrl: 'templates/editPet.html',
-        controller: 'editPetController'
-      }
-    }
+        controller: 'editPetController',
+      },
+    },
   })
 
   .state('app.myPets', {
@@ -112,27 +105,27 @@ angular.module('starter',['ionic',
     views: {
       'menuContent': {
         templateUrl: 'templates/myPets.html',
-        controller: 'MyPetsCtrl'
-      }
-    }
+        controller: 'MyPetsCtrl',
+      },
+    },
   })
 
   .state('login', {
     url: '/login',
     templateUrl: 'templates/login.html',
-    controller: 'AuthController'
+    controller: 'AuthController',
   })
 
   .state('bankAuth', {
     url: '/bankAuth',
     templateUrl: 'templates/bankAuth.html',
-    controller: 'BankCtrl'
+    controller: 'BankCtrl',
   })
 
   .state('signup', {
     url: '/signup',
     templateUrl: 'templates/signup.html',
-    controller: 'AuthController'
+    controller: 'AuthController',
   });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/login');
