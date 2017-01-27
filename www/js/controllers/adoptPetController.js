a7d214a41a1aca6f19f814007ec9aa025f2bd689
Brendan Frost <brendangfrost@gmail.com>
2017-01-27 16:59:49 -0600

    update slider to newest ion slides and fix linter issues w adopt pet ctrl

--- www/js/controllers/adoptPetController.js
+++ www/js/controllers/adoptPetController.js
@@ -1,54 +1,80 @@
+/* eslint no-param-reassign: ["error", { "props": false }] */
 angular.module('app.adoptPet', [])
 
   .controller('adoptPetCtrl', function($scope, $http, $rootScope, $location) {
 
+    $scope.data = {};
     $scope.selectedPet;
     $scope.newpet = {};
     $scope.petTypes = [];
     $scope.petImages = {
       1: '../img/pets/bear.png',
       2: '../img/pets/octopus.png',
-      3: '../img/pets/dragon.png'
+      3: '../img/pets/dragon.png',
     };
 
-    $scope.petTypeDisplay = function(type) {
-      let types = {
-        bear: 'fluffy bear',
-        cat: 'sly octopus',
-        dog: 'fiery dragon'
+    const setupSlider = () => {
+    // some options to pass to our slider
+      $scope.data.sliderOptions = {
+        initialSlide: 0,
+        direction: 'horizontal', // or vertical
+        speed: 300, // 0.3s transition
+        height: 150,
       };
-      return types[type];
+
+      // create delegate reference to link with slider
+      $scope.data.sliderDelegate = null;
+
+      // watch our sliderDelegate reference, and use it when it becomes available
+      $scope.$watch('data.sliderDelegate', (newVal, oldVal) => {
+        if (newVal != null) {
+          $scope.data.sliderDelegate.on('slideChangeEnd', () => {
+            $scope.data.currentPage = $scope.data.sliderDelegate.activeIndex;
+            $scope.selectedPet = $scope.petTypes[$scope.data.sliderDelegate.activeIndex];
+            // use $scope.$apply() to refresh any content external to the slider
+            $scope.$apply();
+          });
+        }
+      });
     };
 
-    $scope.slideHasChanged = function(index) {
-      $scope.selectedPet = $scope.petTypes[index];
-    };
+    setupSlider();
+
+    // $scope.slideHasChanged = function(index) {
+    //   $scope.selectedPet = $scope.petTypes[index];
+    //   console.log($scope.selectedPet, 'selected pet!?!?')
+    // };
 
     $http.get('http://35.167.2.107:3000/v1/pet_types')
-      .then(function(res) {
+      .then(res => {
         res.data.data.forEach(type => {
+          const types = {
+            bear: 'fluffy bear',
+            octopus: 'sly octopus',
+            dragon: 'fiery dragon',
+          };
+
           $scope.petTypes.push(type);
-          for(let i = 0; i < $scope.petTypes.length; i++) {
+          for (let i = 0; i < $scope.petTypes.length; i++) {
             $scope.petTypes[i].img = $scope.petImages[$scope.petTypes[i].id];
-            $scope.selectedPet = $scope.petTypes[0].name;
           }
+          $scope.petTypes.forEach(pet => {
+            pet.displayType = types[pet.name];
+          });
         });
-      }, function(err) {
-        console.log(err);
+      }, err => {
+        console.warn(err);
       });
 
-    $scope.adoptNewPet = function() {
-
+    $scope.adoptNewPet = () => {
       $scope.newpet.pet_type_id = $scope.selectedPet.id || 1;
       $scope.newpet.user_id = $rootScope.user;
 
       $http.post('http://35.167.2.107:3000/v1/pets', $scope.newpet)
-        .then(function() {
+        .then(() => {
           $location.path('/app/myPets');
-        }, function(err) {
-          console.log(err);
+        }, err => {
+          console.warn(err);
         });
-
     };
-
   });
