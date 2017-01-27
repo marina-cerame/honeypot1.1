50a25f68260acdad3564d50ff7f199e365f26148
Brendan Frost <brendangfrost@gmail.com>
2017-01-27 12:43:59 -0600

    correct accessory list error

--- www/js/controllers/storeController.js
+++ www/js/controllers/storeController.js
@@ -21,8 +21,8 @@ angular.module('app.store', [])
     2: '/img/salmon.png',
     3: '/img/honey_pot.png',
     10: '/img/wiz-hat.png',
-    11: '/img/balloons.png',
-    12: '/img/clock-chain.png',
+    11: '/img/clock-chain.png',
+    12: '/img/balloons.png',
     19: '/img/coffee.png',
     20: '/img/chips.png',
     21: '/img/klondike.png'
