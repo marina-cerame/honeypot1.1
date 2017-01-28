c0c1f2578937533e18b19b110a5628a13bcef3fa
Brendan Frost <brendangfrost@gmail.com>
2017-01-28 15:29:16 -0600

    add first pet service and corresponding files

--- www/pages/firstPet/firstPetController.js
+++ www/pages/firstPet/firstPetController.js
@@ -1,22 +1,13 @@
+/* globals angular */
 /* eslint no-param-reassign: ["error", { "props": false }] */
 angular.module('app.firstPet', [])
-.controller('FirstPetCtrl', function ($scope, $location, $http, $rootScope) {
-  $scope.pet = {};
 
-  $scope.bearShrink = () => {
-    TweenLite.to('.first-bear', 2, { scale: 0.75, x: '15%', y: '-40%' })
-  };
-  $scope.bearShrink();
+  .controller('FirstPetCtrl', function ($scope, $location, $http, $rootScope, First) {
+    $scope.pet = {};
 
-  $scope.makeFirstPet = () => {
-    $scope.pet.pet_type_id = 1;
-    $scope.pet.user_id = $rootScope.user;
-    $http.post('http://35.167.2.107:3000/v1/pets', $scope.pet)
-      .then((res) => {
-        $rootScope.pet = res.data.data[0];
-        $location.path('/market/pet');
-      }, (err) => {
-        console.warn(err);
-      });
-  };
-});
+    First.bearShrink();
+
+    $scope.createFirst = () => {
+      First.makeFirstPet($scope.pet);
+    };
+  });
---
+++ www/pages/firstPet/firstPetService.js
@@ -0,0 +1,29 @@
+/* global angular, TweenLite */
+/* eslint no-param-reassign: ["error", { "props": false }] */
+angular.module('firstPetService', [
+  'app.firstPet',
+])
+
+  .factory('First', ($location, $http, $rootScope) => {
+    const bearShrink = () => {
+      TweenLite.to('.first-bear', 2, { scale: 0.75, x: '15%', y: '-40%' });
+    };
+
+    const makeFirstPet = firsty => {
+      const pet = firsty;
+      pet.pet_type_id = 1;
+      pet.user_id = $rootScope.user;
+      $http.post('http://35.167.2.107:3000/v1/pets', pet)
+        .then((res) => {
+          $rootScope.pet = res.data.data[0];
+          $location.path('/market/pet');
+        }, (err) => {
+          console.warn(err);
+        });
+    };
+
+    return {
+      bearShrink,
+      makeFirstPet,
+    };
+  });
