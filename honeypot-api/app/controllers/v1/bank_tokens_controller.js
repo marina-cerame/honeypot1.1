4e73821e893a9a80652d4defc547961720307891
Connor <cgreyparsons@gmail.com>
2017-01-30 16:19:59 -0600

    bankAuth page looks a little more reasonable

--- honeypot-api/app/controllers/v1/bank_tokens_controller.js
+++ honeypot-api/app/controllers/v1/bank_tokens_controller.js
@@ -14,25 +14,17 @@ const plaidClient = new plaid.Client(process.env.PLAID_CLIENT_ID,
 class V1BankTokensController extends Nodal.Controller {
 
   index() {
-
     BankToken.query()
       .where(this.params.query)
       .end((err, models) => {
-
         this.respond(err || models);
-
       });
-
   }
 
   show() {
-
     BankToken.find(this.params.route.id, (err, model) => {
-
       this.respond(err || model);
-
     });
-
   }
 
   create() {
@@ -56,36 +48,70 @@ class V1BankTokensController extends Nodal.Controller {
         if (err != null) {
           console.log(err);
         } else {
-          var access_token = res.access_token;
-          var bank_account_token = res.stripe_bank_account_token;
-          stripe.customers.create({
-            source: bank_account_token
-          }, function(err, customer) {
-            if (err) {
-              console.log(err);
-            } else {
-              const newToken = {
-                user_id: user_id,
-                type: type,
-                name: name,
-                token: customer.id
+          const access_token = res.access_token;
+          const bank_account_token = res.stripe_bank_account_token;
+          if (type === 'checking') {
+            stripe.customers.create({
+              source: bank_account_token
+            }, function(err, customer) {
+              if (err) {
+                console.log(err);
+              } else {
+                const newToken = {
+                  user_id: user_id,
+                  type: type,
+                  name: name,
+                  token: customer.id,
+                }
+                BankToken.create(newToken, (err, model) => {
+                  context.respond(err || model);
+                });
               }
-              BankToken.create(newToken, (err, model) => {
-                context.respond(err || model);
-              });
-            }
-          });
+            });
+          } else {
+            stripe.accounts.create({
+              managed: true,
+              country: 'US',
+              external_account: bank_account_token,
+              legal_entity: {
+                dob: {
+                  day: 26,
+                  month: 2,
+                  year: 1992,
+                },
+                first_name: 'Connor',
+                last_name: 'Parsons',
+                type: 'individual',
+              },
+              tos_acceptance: {
+                date: Math.floor(Date.now() / 1000),
+                ip: '98.172.185.234',
+              },
+            }, function (err, account) {
+              if (err) {
+                console.log(err);
+              } else {
+                const newToken = {
+                  user_id: user_id,
+                  type: type,
+                  name: name,
+                  token: account.id
+                }
+                BankToken.update(context.params.route.id, newToken, (err, model) => {
+                  context.respond(err || model);
+                });
+              }
+            });
+          }
         }
     });
   }
   update() {
-
     // BankToken.update(this.params.route.id, this.params.body, (err, model) => {
     //
     //   this.respond(err || model);
     //
     // });
-    console.log('hit////////////////////');
     const public_token = this.params.body.public_token;
     const account_id = this.params.body.account_id;
     const user_id = this.params.body.user_id;
@@ -98,8 +124,8 @@ class V1BankTokensController extends Nodal.Controller {
         if (err != null) {
           console.log(err);
         } else {
-          var access_token = res.access_token;
-          var bank_account_token = res.stripe_bank_account_token;
+          const access_token = res.access_token;
+          const bank_account_token = res.stripe_bank_account_token;
           if (type === 'checking') {
             stripe.customers.create({
               source: bank_account_token
@@ -111,7 +137,7 @@ class V1BankTokensController extends Nodal.Controller {
                   user_id: user_id,
                   type: type,
                   name: name,
-                  token: customer.id
+                  token: customer.id,
                 }
                 BankToken.update(context.params.route.id, newToken, (err, model) => {
                   context.respond(err || model);
@@ -122,6 +148,21 @@ class V1BankTokensController extends Nodal.Controller {
             stripe.accounts.create({
               managed: true,
               country: 'US',
+              external_account: bank_account_token,
+              legal_entity: {
+                dob: {
+                  day: 26,
+                  month: 2,
+                  year: 1992,
+                },
+                first_name: 'Connor',
+                last_name: 'Parsons',
+                type: 'individual',
+              },
+              tos_acceptance: {
+                date: Math.floor(Date.now() / 1000),
+                ip: '98.172.185.234',
+              },
             }, function (err, account) {
               if (err) {
                 console.log(err);
@@ -130,9 +171,8 @@ class V1BankTokensController extends Nodal.Controller {
                   user_id: user_id,
                   type: type,
                   name: name,
-                  token: account.id
+                  token: account.id,
                 }
-                console.log('account: ', account);
                 BankToken.update(context.params.route.id, newToken, (err, model) => {
                   context.respond(err || model);
                 });
@@ -144,15 +184,10 @@ class V1BankTokensController extends Nodal.Controller {
   }
 
   destroy() {
-
     BankToken.destroy(this.params.route.id, (err, model) => {
-
       this.respond(err || model);
-
     });
-
   }
-
 }
 
 module.exports = V1BankTokensController;
